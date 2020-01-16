700ef3145c130035f401ed99d00fe34f7f2c1eb5
tallesv <talles17.a@hotmail.com>
2020-01-15 21:46:54 -0300

    Construindo a interface web

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 mongoose.set('useCreateIndex', true);
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -10,6 +11,8 @@ mongoose.connect('mongodb+srv://talles:omnistack@cluster0-ieqsy.mongodb.net/week
     useUnifiedTopology: true
 });
 
+
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
